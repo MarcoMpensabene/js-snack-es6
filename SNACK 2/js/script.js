61ec31031252e79a109698ea53f579e53e07380b
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-23 16:09:55 +0200

    Snack- 2 , added array of obj , added 3 list , first mapping with touppercase , the others filter with a condition

---
+++ SNACK 2/js/script.js
@@ -0,0 +1,36 @@
+
+
+const students = [
+    {Id : 213 , Name : "Giuseppina della Rovere" , Grades : 78},
+    {Id : 110 , Name : "Paola Cortellessa" , Grades : 96},
+    {Id : 250 , Name : "Andrea Mantegna" , Grades : 48},
+    {Id : 145 , Name : "Gaia Borromini" , Grades : 74},
+    {Id : 196 , Name : "Luigi Grimaldello" , Grades : 68},
+    {Id : 102 , Name : "Piero della Francesca" , Grades : 50},
+    {Id : 120 , Name : "Francesca da Polenta" , Grades : 84},
+];
+
+console.log(students)
+
+const studentsUpperCase = students.map(studentsUp => {
+    studentsUp.Name = studentsUp.Name.toUpperCase() // > RICORDA DI INSERIRE LE PARENTESI GRAZIE MILLE ME DEL FUTURO 
+    return studentsUp
+})
+
+console.log(studentsUpperCase)
+
+const goodStudents = students.filter(studentsG => {
+    if (studentsG.Grades > 70){
+        return studentsG
+    }
+})
+
+console.log(goodStudents)
+
+const goodStudentsId = students.filter(studentsGiD => {
+    if (studentsGiD.Grades > 70 && studentsGiD.Id > 120){
+        return studentsGiD
+    }
+})
+
+console.log(goodStudentsId)
